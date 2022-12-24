ae65589967dd4649b491a5b9140c622c35855f9e
Jeart Thaqi <jeartt.th@gmail.com>
2022-12-24 12:20:07 +0100

    First task done!

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
-st.header('This is a header')
-st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
+st.header('This is the project for Bit Academy')
+# st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
+
+testim = st.text_input('This is the text input')
+st.text(testim)
