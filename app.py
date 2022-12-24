7346419c0ed64010878e4057ea3ed954ef20adc5
Jeart Thaqi <jeartt.th@gmail.com>
2022-12-24 13:08:45 +0100

    Cleared some of the unneeded comments!

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 st.header('This is the project for Bit Academy')
-# st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
 
-# First task
+# Task 1
 mainString = st.text_input('This is the text input')
-# st.text(mainString)
+
 b = None
 
 def convert_list(mainString):
@@ -16,24 +15,26 @@ if st.button("Return List"):
     b = convert_list(mainString)
     st.write(b)
 
-# Declare a function called “convert_list”  that takes this string and returns a list of words from this string. (hint: you can use .split() method ). 
-# Connect this function with a button called “Return List”. When you press this button list of words is printed on streamlit.
-# (hint: you can use st.write())
-
-
+# Task 3
 def convert_upper():
     b = convert_list(mainString)
     b = [a.upper() for a in b]
-    st.text(b)
+    return b
 
 if st.button("Upper"):
-    convert_upper()
-    # st.write(test)
+    c = convert_upper()
+    st.write(c)
 
-# (A) Declare another function called “convert_upper” that takes the list from the function called “convert_list” and converts them to a list with all upper case.
-# (hint: you need to use for loop, .upper() and .append() methods ). Connect this function to a button called “upper” that activates this function and prints te result.
-# (hint: you need to use st.write()
 
+# Task 4
+def count():
+    lista = convert_list(mainString)
+    lista = len(lista)
+    return lista
+
+if st.button("print_count"):
+    d = count()
+    st.write("This list has ", d, " elements")
 
 
 # This is just to hide the header and the footer
