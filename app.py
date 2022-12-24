9a36b08f29ba4ca712536956e17977fa2b481c77
Jeart Thaqi <jeartt.th@gmail.com>
2022-12-24 12:26:49 +0100

    Added sum code to remove the header and footer of default streamlit

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ import streamlit as st
 st.header('This is the project for Bit Academy')
 # st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
 
+# First task
 testim = st.text_input('This is the text input')
 st.text(testim)
+
+# This is just to hide the header and the footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
